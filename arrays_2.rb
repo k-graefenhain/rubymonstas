6805a5b82ac184009407de96e857cbe5d76519de
k-graefenhain <katja.graefenhain@ergon.ch>
2015-06-19 10:22:34 +0200

    array exercises

--- arrays_2.rb
+++ arrays_2.rb
@@ -10,5 +10,51 @@ words = ["one", "two", "three", "four", "five"]
 
 words.select!.with_index { |word, i| i.even? }
 words.map! { |word| word.capitalize}
+#words.map! { |word| "%s <3" % word }
+
+words.map!.with_index do |word, i| 
+  hearts = "<3" * (i*2 + 1)
+  "#{word} \t #{hearts}"
+end
 
 p words
+
+# words.each do |word|
+#   puts word
+# end
+
+puts words.join("\n")
+
+
+###########################
+array = [1,2,3,4,5]
+p array.shift
+p array.shift
+p array
+
+
+
+def merge_arrays_old(a1, a2)
+  res = []
+  a1.each do |e1|
+    if a2.include?e1
+      while (e2 = a2.shift) != e1
+        res << e2
+      end
+    end
+    res << e1
+  end
+  res.concat(a2)
+end
+
+def merge_arrays(a1, a2)
+  a1 | a2
+end
+
+a = [1,2,3,4,5]
+b = [1,6,7,8,9,10,11,12,13,14]
+
+#p merge_arrays(a,b)
+#p a | b
+#p merge_arrays(b,a)
+#p b | a
